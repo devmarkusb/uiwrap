6a1f4fc329533e538d48c5673489efa3c7971c54
MarkusB <markusb81@web.de>
2016-01-29 23:18:36 +0100

    cmake build config for Qt (Widgets sufficient so far), uiwrap now successfully compiles for Qt impl. choice

--- include/uiwrap/linklib/impl_Qt/LINKLIB_DEFS_Qt.h
+++ include/uiwrap/linklib/impl_Qt/LINKLIB_DEFS_Qt.h
@@ -9,6 +9,8 @@
 #ifndef LINKLIB_DEFS_QT_H_INCL_skdhoikmhcnvncynmxcof78s7
 #define LINKLIB_DEFS_QT_H_INCL_skdhoikmhcnvncynmxcof78s7
 
+#include <QtCore/QtGlobal>
+
 
 #define UIW_DECL_EXPORT Q_DECL_EXPORT
 #define UIW_DECL_IMPORT Q_DECL_IMPORT
