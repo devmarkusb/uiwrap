a5adf8b0efa4d1268ec9482715a7a7f42dba9cd0
MarkusB <markusb81@web.de>
2016-12-19 01:22:45 +0100

    UrlString_util_Qt: major improvement, better understanding of the Qt url prefix schemes

--- include/uiwrap/string/impl_Qt/UrlString_util_Qt.h
+++ include/uiwrap/string/impl_Qt/UrlString_util_Qt.h
@@ -8,8 +8,12 @@
 
 #ifndef URLSTRING_UTIL_QT_H_sduigfxn3r7ngf27823f
 #define URLSTRING_UTIL_QT_H_sduigfxn3r7ngf27823f
+//#ifdef UIW_LINKLIB_IMPL_CHOICE_QT
 
+#include "Toolib/PPDefs/MSVC/SUPPRESS_WARNINGS_EXTERNAL_BEGIN"
 #include <QString>
+#include <QUrl>
+#include "Toolib/PPDefs/MSVC/SUPPRESS_WARNINGS_EXTERNAL_END"
 
 
 namespace uiw
@@ -17,16 +21,30 @@ namespace uiw
 namespace implQt
 {
 //! \param abs_path needs to be an absolut path of a file or dir.
-//! This function prepends something like `file:///`.
+//! This function prepends something like `file:///`. This is called the
+//! scheme and can be 'ftp' or 'http' and such, all appended with '://'.
+//! Probably the third slash '/' belongs to the path itself for the
+//! 'file' case.
 //! We don't make any suggestion at this place of whether you will need
 //! native separators or not. There are appropriate conversion functions.
-inline QString prependFileDir_urlPrefix(const QString& abs_path)
+inline QString prependFile_urlSchemePrefix(const QString& abs_path)
 {
-    QString ret{abs_path};
-    ret.insert(0, QStringLiteral("file:///"));
-    return ret;
+    QUrl u{QUrl::fromLocalFile(abs_path)};
+    return u.toString();
+}
+
+//! Inverse to prependFile_urlSchemePrefix, this removes the prefixed scheme.
+inline QString removeFile_urlSchemePrefix(const QUrl& abs_path_with_scheme_prefix)
+{
+    return abs_path_with_scheme_prefix.toLocalFile();
+}
+inline QString removeFile_urlSchemePrefix(const QString& abs_path_with_scheme_prefix)
+{
+    const QUrl u{QUrl::fromLocalFile(abs_path_with_scheme_prefix)};
+    return removeFile_urlSchemePrefix(u);
 }
 }
 }
 
+//#endif //UIW_LINKLIB_IMPL_CHOICE_QT
 #endif
