2150ed162c92ca3fda6b05761c8f65349093fb92
MarkusB <markusb81@web.de>
2017-01-05 20:57:26 +0100

    resources: added string type id

--- include/uiwrap/GUI/resources.h
+++ include/uiwrap/GUI/resources.h
@@ -9,13 +9,17 @@
 #ifndef RESOURCES_H_sngfxzgunyfz73i6rg4yrgfn3f723gfv4f
 #define RESOURCES_H_sngfxzgunyfz73i6rg4yrgfn3f723gfv4f
 
+#include <string>
+
 
 namespace uiw
 {
 namespace gui
 {
-typedef long ResourceID;
-}
-}
+using ResourceID = long;
+//! Alpanumeric variant.
+using ResourceID_alnum = std::string;
+} // gui
+} // uiw
 
 #endif
