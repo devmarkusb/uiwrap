c6bcdb137b35d863543e640d7e2b177e193546c3
MarkusB <markusb81@web.de>
2017-02-11 10:26:58 +0100

    fixes last commit; also late updated the qt-win-deployment to new handling (would have been needed for a stand-alone uiwrap build only so far)

--- qt.cmake
+++ qt.cmake
@@ -4,8 +4,12 @@
 #   find_package(Qt5Widgets)
 #   find_package(Qt5Qml)
 #   # ...
-set(Qt5_version         "5.8" CACHE STRING "string has to match the subdir under Qt install location, like 5.7 or 5.8")
-set(Qt_compiler_subdir  "mingw53_32" CACHE STRING "string has to match the subdir under Qt version dir of the install location, like mingw53_32 or msvc2013")
+
+# Note: it's important not to give default values here. Otherwise the configuration could
+# succeed in a wrong way. Also if you like to change these parameters later on you would have to delete
+# and rebuild the CMake cache. Reason: Qt package finding should start from scratch.
+set(Qt5_version         "" CACHE STRING "string has to match the subdir under Qt install location, like 5.7 or 5.8")
+set(Qt_compiler_subdir  "" CACHE STRING "string has to match the subdir under Qt version dir of the install location, like mingw53_32 or msvc2013")
 
 set(CMAKE_INCLUDE_CURRENT_DIR ON)
 set(CMAKE_AUTOMOC ON)
