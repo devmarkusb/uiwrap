6e98723a5f3172c723df5b938da4ae3c49255f45
MarkusB <markusb81@web.de>
2017-02-11 21:05:41 +0100

    use Toolib with boost as well

--- boost.cmake
+++ boost.cmake
@@ -1,5 +1,6 @@
 # Include this file and just add e.g.
 #   find_package(Boost 1.63.0 COMPONENTS system filesystem) # or similar
+# Then include ${Boost_INCLUDE_DIRS} and link ${Boost_LIBRARIES} if necessary.
 set(BOOST_ROOT $ENV{dev_sdk_path})
 set(Boost_USE_MULTITHREADED     ON)
 set(Boost_USE_STATIC_LIBS       OFF)
