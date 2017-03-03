4c7e975bcef85ebe5f32231b89b12471e9d253f7
MarkusB <markusb81@web.de>
2017-03-03 23:39:20 +0100

    clang warning fixes

--- src/filesys/impl_Qt/filesys_Qt.h
+++ src/filesys/impl_Qt/filesys_Qt.h
@@ -24,21 +24,21 @@ namespace implQt
 class UIWRAPSHARED_EXPORT CFileSys_Qt : public uiw::IFileSys
 {
 public:
-    virtual bool SaveToTextFile(const std::string& filePathNameExt, const std::string& content);
-    virtual bool LoadFromTextFile(const std::string& filePathNameExt, std::string& content) const;
-    virtual bool CopyFile(const std::string& filePathNameExt_From, const std::string& filePathNameExt_To);
-    virtual bool DeleteFile(const std::string& filePathNameExt);
-    virtual bool RenameFile(const std::string& filePathNameExt_From, const std::string& filePathNameExt_To);
-    virtual bool CreateFolder(const std::string& folderPath);
-    virtual bool DeleteFolder(const std::string& folderPath);
-    virtual bool RenameFolder(const std::string& folderPath_From, const std::string& folderPath_To);
-    virtual bool FolderExists(const std::string& folderPath) const;
-    virtual bool FileExists(const std::string& filePathNameExt) const;
-    virtual bool isFile(const std::string& filePathNameExt) const;
+    virtual bool SaveToTextFile(const std::string& filePathNameExt, const std::string& content) override;
+    virtual bool LoadFromTextFile(const std::string& filePathNameExt, std::string& content) const override;
+    virtual bool CopyFile(const std::string& filePathNameExt_From, const std::string& filePathNameExt_To) override;
+    virtual bool DeleteFile(const std::string& filePathNameExt) override;
+    virtual bool RenameFile(const std::string& filePathNameExt_From, const std::string& filePathNameExt_To) override;
+    virtual bool CreateFolder(const std::string& folderPath) override;
+    virtual bool DeleteFolder(const std::string& folderPath) override;
+    virtual bool RenameFolder(const std::string& folderPath_From, const std::string& folderPath_To) override;
+    virtual bool FolderExists(const std::string& folderPath) const override;
+    virtual bool FileExists(const std::string& filePathNameExt) const override;
+    virtual bool isFile(const std::string& filePathNameExt) const override;
 
-    virtual std::string toNativeSeparators(const std::string& Path) const;
+    virtual std::string toNativeSeparators(const std::string& Path) const override;
 
-    virtual bool GetSystemPath(ESysPathType Type, std::string& Path, bool WithTrailingSeparator) const;
+    virtual bool GetSystemPath(ESysPathType Type, std::string& Path, bool WithTrailingSeparator) const override;
 
     virtual std::string getErrorOfLatestCall() const override;
 
