719d04404e8fa3eaed00bc8c2725862448b27baf
MarkusB <>
2026-05-17 10:11:02 +0200

    tidy

---
+++ src/programsettings/impl_/ProgSettings_.cpp
@@ -0,0 +1,132 @@
+#include "ProgSettings_.h"
+
+#include "mb/ul/ul.hpp"
+UL_PRAGMA_WARNINGS_PUSH_AND_DISABLE_ALL_MSVC
+UL_WARNING_DISABLE_GCC(unused-local-typedefs)
+#include "boost/property_tree/info_parser.hpp"
+#include "boost/property_tree/ini_parser.hpp"
+#include "boost/property_tree/json_parser.hpp"
+#include "boost/property_tree/xml_parser.hpp"
+UL_PRAGMA_WARNINGS_POP
+
+namespace mb::uiw::impl {
+CProgSettings::CProgSettings(
+    const std::string& fileNamePath, const std::string& fileExt, StorageFileFormat preferredStorageFileFormat)
+    : m_StorageFileFormat{preferredStorageFileFormat}
+    , m_FileName{concatenateWithHierarchySep(fileNamePath, fileExt)} {
+}
+
+CProgSettings::~CProgSettings() {
+    try {
+        persistToFile();
+    } catch (...) {
+    }
+}
+
+std::string CProgSettings::concatenateWithHierarchySep(const std::string& first, const std::string& second) {
+    std::string ret{first};
+    ret.append(HIERARCHY_SEPARATOR);
+    ret += second;
+    return ret;
+}
+
+void CProgSettings::init(const std::string&, const std::string&) {
+    try {
+        // NOLINTBEGIN(bugprone-branch-clone) -- distinct boost::property_tree readers per format
+        switch (m_StorageFileFormat) {
+            case StorageFileFormat::INI:
+                boost::property_tree::read_ini(m_FileName, m_PropTree);
+                break;
+            case StorageFileFormat::XML:
+                boost::property_tree::read_xml(m_FileName, m_PropTree);
+                break;
+            case StorageFileFormat::JSON:
+                boost::property_tree::read_json(m_FileName, m_PropTree);
+                break;
+            case StorageFileFormat::BOOST_INFO:
+                boost::property_tree::read_info(m_FileName, m_PropTree);
+                break;
+            default:
+                return;
+        }
+        // NOLINTEND(bugprone-branch-clone)
+    } catch (...) {
+        SetError(EError::ERROR_READING_SETTINGS);
+        return;
+    }
+    m_FirstOccurredError = EError::E_NO_ERROR;
+}
+
+std::string CProgSettings::valueStr(const std::string&, const std::string&, const std::string&) const {
+    throw ul::NotImplemented{"valueStr"};
+}
+
+void CProgSettings::setValueStr(const std::string&, const std::string&, const std::string&) {
+    throw ul::NotImplemented("setValueStr");
+}
+
+std::vector<IProgSettings::TSectionKeyPair> CProgSettings::getAllKeys() const {
+    std::vector<IProgSettings::TSectionKeyPair> ret;
+    ret.reserve(m_PropTree.size());
+    for (const auto& path : m_PropTree) {
+        ret.emplace_back(std::string(), path.first);
+    }
+    return ret;
+}
+
+void CProgSettings::clear() {
+    m_PropTree.clear();
+}
+
+void CProgSettings::enable(bool) {
+    throw ul::NotImplemented("enable");
+}
+
+bool CProgSettings::contains(const std::string& sectionName, const std::string& keyName) const {
+    const std::string path{concatenateWithHierarchySep(sectionName, keyName)};
+    return m_PropTree.find(path) != m_PropTree.not_found();
+}
+
+void CProgSettings::remove(const std::string& sectionName, const std::string& keyName) {
+    const std::string path{concatenateWithHierarchySep(sectionName, keyName)};
+    m_PropTree.erase(path);
+}
+
+void CProgSettings::sync() {
+    persistToFile();
+}
+
+void CProgSettings::persistToFile() {
+    try {
+        // NOLINTBEGIN(bugprone-branch-clone) -- distinct boost::property_tree writers per format
+        switch (m_StorageFileFormat) {
+            case StorageFileFormat::INI:
+                boost::property_tree::write_ini(m_FileName, m_PropTree);
+                break;
+            case StorageFileFormat::XML:
+                boost::property_tree::write_xml(m_FileName, m_PropTree);
+                break;
+            case StorageFileFormat::JSON:
+                boost::property_tree::write_json(m_FileName, m_PropTree);
+                break;
+            case StorageFileFormat::BOOST_INFO:
+                boost::property_tree::write_info(m_FileName, m_PropTree);
+                break;
+            default:
+                return;
+        }
+        // NOLINTEND(bugprone-branch-clone)
+    } catch (...) {
+        SetError(EError::ERROR_WRITING_SETTINGS);
+        return;
+    }
+}
+
+IProgSettings::EError CProgSettings::getError() const {
+    return m_FirstOccurredError;
+}
+
+void CProgSettings::resetError() {
+    m_FirstOccurredError = EError::E_NO_ERROR;
+}
+} // namespace mb::uiw::impl
--- src/programsettings/impl_/ProgSettings_.h
+++ src/programsettings/impl_/ProgSettings_.h
@@ -5,17 +5,12 @@
 #ifndef PROGSETTINGS_H_ieuhrngt783znt7238t87t3
 #define PROGSETTINGS_H_ieuhrngt783znt7238t87t3
 
-#include "mb/ul/ul.hpp"
 #include "uiwrap/programsettings/programsettings.h"
 UL_PRAGMA_WARNINGS_PUSH_AND_DISABLE_ALL_MSVC
 UL_WARNING_DISABLE_GCC(unused-local-typedefs)
-#include "boost/property_tree/info_parser.hpp"
-#include "boost/property_tree/ini_parser.hpp"
-#include "boost/property_tree/json_parser.hpp"
 #include "boost/property_tree/ptree.hpp"
-#include "boost/property_tree/xml_parser.hpp"
 UL_PRAGMA_WARNINGS_POP
-#include <memory>
+#include <string>
 
 namespace mb::uiw::impl {
 namespace boost_pt = boost::property_tree;
@@ -60,129 +55,6 @@ private:
     //! first and second mustn't start or end with HIERARCHY_SEPARATOR
     static std::string concatenateWithHierarchySep(const std::string& first, const std::string& second);
 };
-
-//####################################################################################################################
-
-CProgSettings::CProgSettings(
-    const std::string& fileNamePath, const std::string& fileExt, StorageFileFormat preferredStorageFileFormat)
-    : m_StorageFileFormat{preferredStorageFileFormat}
-    , m_FileName{concatenateWithHierarchySep(fileNamePath, fileExt)} {
-}
-
-CProgSettings::~CProgSettings() {
-    try {
-        persistToFile();
-    } catch (...) {
-    }
-}
-
-std::string CProgSettings::concatenateWithHierarchySep(const std::string& first, const std::string& second) {
-    std::string ret{first};
-    ret.append(HIERARCHY_SEPARATOR);
-    ret += second;
-    return ret;
-}
-
-void CProgSettings::init(const std::string&, const std::string&) {
-    try {
-        // NOLINTBEGIN(bugprone-branch-clone) -- distinct boost::property_tree readers per format
-        switch (m_StorageFileFormat) {
-            case StorageFileFormat::INI:
-                boost::property_tree::read_ini(m_FileName, m_PropTree);
-                break;
-            case StorageFileFormat::XML:
-                boost::property_tree::read_xml(m_FileName, m_PropTree);
-                break;
-            case StorageFileFormat::JSON:
-                boost::property_tree::read_json(m_FileName, m_PropTree);
-                break;
-            case StorageFileFormat::BOOST_INFO:
-                boost::property_tree::read_info(m_FileName, m_PropTree);
-                break;
-            default:
-                return;
-        }
-        // NOLINTEND(bugprone-branch-clone)
-    } catch (...) {
-        SetError(EError::ERROR_READING_SETTINGS);
-        return;
-    }
-    m_FirstOccurredError = EError::E_NO_ERROR;
-}
-
-std::string CProgSettings::valueStr(const std::string&, const std::string&, const std::string&) const {
-    throw ul::NotImplemented{"valueStr"};
-}
-
-void CProgSettings::setValueStr(const std::string&, const std::string&, const std::string&) {
-    throw ul::NotImplemented("setValueStr");
-}
-
-// NOLINTNEXTLINE(readability-convert-member-functions-to-static) -- uses m_PropTree
-std::vector<IProgSettings::TSectionKeyPair> CProgSettings::getAllKeys() const {
-    std::vector<IProgSettings::TSectionKeyPair> ret;
-    ret.reserve(m_PropTree.size());
-    for (const auto& path : m_PropTree) {
-        ret.emplace_back(std::string(), path.first);
-    }
-    return ret;
-}
-
-void CProgSettings::clear() {
-    m_PropTree.clear();
-}
-
-void CProgSettings::enable(bool) {
-    throw ul::NotImplemented("enable");
-}
-
-bool CProgSettings::contains(const std::string& sectionName, const std::string& keyName) const {
-    const std::string path{concatenateWithHierarchySep(sectionName, keyName)};
-    return m_PropTree.find(path) != m_PropTree.not_found();
-}
-
-void CProgSettings::remove(const std::string& sectionName, const std::string& keyName) {
-    const std::string path{concatenateWithHierarchySep(sectionName, keyName)};
-    m_PropTree.erase(path);
-}
-
-void CProgSettings::sync() {
-    persistToFile();
-}
-
-void CProgSettings::persistToFile() {
-    try {
-        // NOLINTBEGIN(bugprone-branch-clone) -- distinct boost::property_tree writers per format
-        switch (m_StorageFileFormat) {
-            case StorageFileFormat::INI:
-                boost::property_tree::write_ini(m_FileName, m_PropTree);
-                break;
-            case StorageFileFormat::XML:
-                boost::property_tree::write_xml(m_FileName, m_PropTree);
-                break;
-            case StorageFileFormat::JSON:
-                boost::property_tree::write_json(m_FileName, m_PropTree);
-                break;
-            case StorageFileFormat::BOOST_INFO:
-                boost::property_tree::write_info(m_FileName, m_PropTree);
-                break;
-            default:
-                return;
-        }
-        // NOLINTEND(bugprone-branch-clone)
-    } catch (...) {
-        SetError(EError::ERROR_WRITING_SETTINGS);
-        return;
-    }
-}
-
-IProgSettings::EError CProgSettings::getError() const {
-    return m_FirstOccurredError;
-}
-
-void CProgSettings::resetError() {
-    m_FirstOccurredError = EError::E_NO_ERROR;
-}
 } // namespace mb::uiw::impl
 
 #endif
