03f655f32ff81683dc92c7fd2f074de1387e4220
MarkusB <>
2026-05-17 09:52:58 +0200

    tidy

--- src/programsettings/impl_/ProgSettings_.h
+++ src/programsettings/impl_/ProgSettings_.h
@@ -15,10 +15,7 @@ UL_WARNING_DISABLE_GCC(unused-local-typedefs)
 #include "boost/property_tree/ptree.hpp"
 #include "boost/property_tree/xml_parser.hpp"
 UL_PRAGMA_WARNINGS_POP
-#include <algorithm>
-#include <iterator>
 #include <memory>
-#include <utility>
 
 namespace mb::uiw::impl {
 namespace boost_pt = boost::property_tree;
@@ -62,16 +59,6 @@ private:
 
     //! first and second mustn't start or end with HIERARCHY_SEPARATOR
     static std::string concatenateWithHierarchySep(const std::string& first, const std::string& second);
-
-    template <typename ReadFn>
-    void readStorageFile(ReadFn&& readFn) {
-        std::forward<ReadFn>(readFn)(m_FileName, m_PropTree);
-    }
-
-    template <typename WriteFn>
-    void writeStorageFile(WriteFn&& writeFn) {
-        std::forward<WriteFn>(writeFn)(m_FileName, m_PropTree);
-    }
 };
 
 //####################################################################################################################
@@ -98,22 +85,24 @@ std::string CProgSettings::concatenateWithHierarchySep(const std::string& first,
 
 void CProgSettings::init(const std::string&, const std::string&) {
     try {
+        // NOLINTBEGIN(bugprone-branch-clone) -- distinct boost::property_tree readers per format
         switch (m_StorageFileFormat) {
             case StorageFileFormat::INI:
-                readStorageFile(boost::property_tree::read_ini);
+                boost::property_tree::read_ini(m_FileName, m_PropTree);
                 break;
             case StorageFileFormat::XML:
-                readStorageFile(boost::property_tree::read_xml);
+                boost::property_tree::read_xml(m_FileName, m_PropTree);
                 break;
             case StorageFileFormat::JSON:
-                readStorageFile(boost::property_tree::read_json);
+                boost::property_tree::read_json(m_FileName, m_PropTree);
                 break;
             case StorageFileFormat::BOOST_INFO:
-                readStorageFile(boost::property_tree::read_info);
+                boost::property_tree::read_info(m_FileName, m_PropTree);
                 break;
             default:
                 return;
         }
+        // NOLINTEND(bugprone-branch-clone)
     } catch (...) {
         SetError(EError::ERROR_READING_SETTINGS);
         return;
@@ -133,10 +122,9 @@ void CProgSettings::setValueStr(const std::string&, const std::string&, const st
 std::vector<IProgSettings::TSectionKeyPair> CProgSettings::getAllKeys() const {
     std::vector<IProgSettings::TSectionKeyPair> ret;
     ret.reserve(m_PropTree.size());
-    std::transform(
-        m_PropTree.begin(), m_PropTree.end(), std::back_inserter(ret), [](const boost_pt::ptree::value_type& path) {
-            return IProgSettings::TSectionKeyPair{std::string(), path.first};
-        });
+    for (const auto& path : m_PropTree) {
+        ret.emplace_back(std::string(), path.first);
+    }
     return ret;
 }
 
@@ -164,22 +152,24 @@ void CProgSettings::sync() {
 
 void CProgSettings::persistToFile() {
     try {
+        // NOLINTBEGIN(bugprone-branch-clone) -- distinct boost::property_tree writers per format
         switch (m_StorageFileFormat) {
             case StorageFileFormat::INI:
-                writeStorageFile(boost::property_tree::write_ini);
+                boost::property_tree::write_ini(m_FileName, m_PropTree);
                 break;
             case StorageFileFormat::XML:
-                writeStorageFile(boost::property_tree::write_xml);
+                boost::property_tree::write_xml(m_FileName, m_PropTree);
                 break;
             case StorageFileFormat::JSON:
-                writeStorageFile(boost::property_tree::write_json);
+                boost::property_tree::write_json(m_FileName, m_PropTree);
                 break;
             case StorageFileFormat::BOOST_INFO:
-                writeStorageFile(boost::property_tree::write_info);
+                boost::property_tree::write_info(m_FileName, m_PropTree);
                 break;
             default:
                 return;
         }
+        // NOLINTEND(bugprone-branch-clone)
     } catch (...) {
         SetError(EError::ERROR_WRITING_SETTINGS);
         return;
