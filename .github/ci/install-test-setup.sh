cfb69686a1f1295cfd6e2941e4a1b65e0d57c3ca
MarkusB <>
2026-05-25 01:37:00 +0200

    ci: fix install

---
+++ .github/ci/install-test-setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+./devenv/install-boost.sh --components property-tree
+
+git clone --depth 1 https://github.com/devmarkusb/util.git /tmp/mb-util
+cmake \
+  -S /tmp/mb-util \
+  -B /tmp/mb-util-build \
+  -DCMAKE_PROJECT_TOP_LEVEL_INCLUDES=./devenv/cmake/fetch-content-from-lockfile.cmake \
+  -DMB_UTIL_BUILD_TESTS=OFF \
+  -DMB_UTIL_BUILD_EXAMPLES=OFF \
+  -DCMAKE_INSTALL_PREFIX=/tmp/mb-util-install
+cmake --build /tmp/mb-util-build --parallel
+cmake --install /tmp/mb-util-build
+
+echo "CMAKE_PREFIX_PATH=/tmp/mb-util-install:${CMAKE_PREFIX_PATH:-}" >> "${GITHUB_ENV}"
